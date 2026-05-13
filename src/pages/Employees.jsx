de553984d2895edaf7dbca0d9363312ff159e11b
CZxix <angelclarkmartir@gmail.com>
2026-05-14 02:09:17 +0800

    feat: implement basic Employee directory UI with table

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,13 @@
+import AppShell from "../../layouts/AppShell";
+
+function EmployeePage() {
+  return (
+    <AppShell>
+      <h1 className="text-2xl font-bold">
+        Employees Page
+      </h1>
+    </AppShell>
+  );
+}
+
+export default EmployeePage;
