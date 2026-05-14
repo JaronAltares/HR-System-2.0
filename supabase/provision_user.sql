e16e442342bbbe87013e4d89c3df48ca1a47daa0
Fred Sapinoro <fredrickjohnsapinoro@gmail.com>
2026-05-14 08:39:58 +0800

    fix: update trigger to use BEFORE INSERT

--- supabase/provision_user.sql
+++ supabase/provision_user.sql
@@ -3,17 +3,15 @@ CREATE OR REPLACE FUNCTION public.provision_new_user()
 RETURNS trigger AS $$
 BEGIN
   -- Set role to 'USER' and status to 'INACTIVE' by default
-  UPDATE auth.users 
-  SET raw_app_meta_data = jsonb_build_object(
-    'role', 'USER', 
+  NEW.raw_app_meta_data = jsonb_build_object(
+    'role', 'USER',
     'record_status', 'INACTIVE'
-  )
-  WHERE id = NEW.id;
+  );
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- Trigger runs automatically after every new signup
 CREATE OR REPLACE TRIGGER on_auth_user_created
-  AFTER INSERT ON auth.users
+  BEFORE INSERT ON auth.users
   FOR EACH ROW EXECUTE PROCEDURE public.provision_new_user();
