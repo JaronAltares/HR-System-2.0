80365b15f853fe2b62a86efdd934d96516a7a542
CZxix <angelclarkmartir@gmail.com>
2026-05-16 21:45:29 +0800

    feat: build Register page UI with validation and Google button placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* This line forces the app to go straight to login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
